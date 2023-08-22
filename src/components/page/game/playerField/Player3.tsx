3be9dac8a3b8fe96dbbe8a1c43a9ab5b5434ab3d
shinequasar <shinequasar@gmail.com>
2023-08-23 00:23:53 +0900

    feat : 플레이어 틀 생성

---
+++ src/components/page/game/playerField/Player3.tsx
@@ -0,0 +1,64 @@
+import PlayerImg from "/assets/players/player.png"
+import styled from "styled-components";
+
+type PlayerProps = {
+    point : number
+    lank : number
+}
+const Player = (props : PlayerProps) => {
+
+    return (
+        <Container>
+            <Inner>
+                <LankMark>{props.lank}</LankMark>
+                <NickName>닉네임</NickName>
+            </Inner>
+            <Img src={PlayerImg}></Img>
+            <Point>{props.point}점</Point>
+        </Container>
+    )
+}
+
+export default Player;
+
+const Container = styled.div`
+  display: block;
+`;
+const Inner = styled.div`
+  display: flex;
+  align-items: end;
+  padding-bottom: 5px;
+`;
+const LankMark = styled.div`
+  background-color: #f37330;
+  width: 25px;
+  height: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  color: white;
+  font-size: 22px;
+  clip-path: polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%);
+`;
+const Img = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const Point = styled.div`
+  background-color: #9df556;
+  border-radius: 10px;
+  font-size: 20px;
+  padding: 3px;
+  width: 80px;
+  margin: 0 auto;
+`;
+const NickName = styled.p`
+  color: white;
+  font-size: 20px;
+  margin: 2px auto;
+  background-color: rgba(0, 0, 0, 0.3);
+  border-radius: 20px;
+  padding: 2px 5px;
+`;
